68a498626495f90a769c413be891ad06819f25f0
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-24 13:00:55 +0330

    ref star

--- DesignPatterns/DesignPatterns.cpp
+++ DesignPatterns/DesignPatterns.cpp
@@ -11,11 +11,11 @@ public:
 
 int main()
 {
-    int* number = new int(53);
+    int* number = new int[2]{53, 71};
     auto ptr = &number;
-    auto item = ***&ptr;
-    std::cout << item << std::endl;
-    delete number;
+    auto item = &**ptr;
+    std::cout << item[1] << std::endl;
+    delete[] number;
     return 0;
 }
 
