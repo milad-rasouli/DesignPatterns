87690510153d89dac1165d5638dc9ca2cc220663
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-25 14:55:30 +0330

    Factory Pattern

--- FactoryPattern/FacoryPattern.cpp
+++ FactoryPattern/FacoryPattern.cpp
@@ -2,31 +2,25 @@
 
 /* main objects */
 class Developer {
+public:
 	std::string name;
 	std::string type;
-public:
 	Developer(std::string const& name, std::string const& type) {
 		this->name = name;
 		this->type = type;
 	}
 
-	auto say()->void const {
-		std::clog << "Name:" << name << " Type:" << type << "\n";
-	}
 };
 
 class Tester {
+public:
 	std::string name;
 	std::string type;
-public:
 	Tester(std::string const& name, std::string const& type) {
 		this->name = name;
 		this->type = type;
 	}
 
-	auto say()->void const{
-		std::clog << "Name:" << name << " Type:" << type << "\n";
-	}
 };
 
 /* factory method */
@@ -43,16 +37,22 @@ public:
 int main()
 {
 
-
+	
 	auto dev1 = EmployeeFactory::create("Sara", "developer");
 	auto dev2 = EmployeeFactory::create("Milad", "developer");
-	
+	auto devs = { std::move(dev1),std::move(dev2) };
+
 	auto test1 = EmployeeFactory::create("Jean", "tester");
 	auto test2 = EmployeeFactory::create("Alex", "tester");
+	auto testers= { std::move(test1),std::move(test2) };
 
-	dev1->say();
-	dev2->say();
-	test1->say();
-	test2->say();
+	auto show = [](auto items) {
+		// if it was shared_ptr we dont need to use &
+		for (auto& item : items) {
+			std::clog << "Name: " << item->name << "\tType: " << item->type << "\n";
+		}
+	};
+	show(devs);
+	show(testers);
 }
 
